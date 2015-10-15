724ce858351e85e849d48eb0c2277ff4089278c7
Yuchao LI <sxtgmxlych@gmail.com>
2015-10-15 16:32:30 +0200

    Matlab script
    Matlab files start up

--- setPath.m
+++ setPath.m
@@ -1,2 +1,2 @@
-javaaddpath('/home/alexander/Documents/ResearchProject/DiDi/Includes/External/jgraphx/lib/jgraphx.jar');
-javaaddpath('/home/alexander/Documents/ResearchProject/DiDi/Includes/External/customjgraphx/customjgraphx/dist/customjgraphx.jar');
+javaaddpath('D:\Documents\MATLAB Files\DiDi\Includes\External\jgraphx\lib\jgraphx.jar');
+javaaddpath('D:\Documents\MATLAB Files\DiDi\Includes\External\customjgraphx\customjgraphx\dist\customjgraphx.jar');
