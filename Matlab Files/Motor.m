546ad296f4705cad01718d309489a1d7e7a51fee
Alexander <gratner@kth.se>
2015-11-02 22:15:26 +0100

    Made changes in 4 files
    Put data into a single file

--- Matlab Files/Motor.m
+++ Matlab Files/Motor.m
@@ -1,20 +1,7 @@
-clear all; close all; clc;
-Cm = 91350;                         %Machine type specific constant, torque [N/m^2.5]
-Cmj = 1056;                         %Machine type specific constant, intertia [kg/m^3]
-Cgr = 1.2;                          %Outer radius by reference radious [~]
-Jload=1.1;                          %Inertia load [kg*m^2]
-inertia_ratio = 10/9;               %(Jm+J0)/Jm
-Trms = 60.9338/0.95;                       %RMS Torque [Nm]
-% a = 2.2*Cmj/(Jload*Trms^2);         %Our introduced variable [...]
-Tpeak = 221.937/0.95;                    %Peak torque [Nm]
-delta_hlim = 1200e6;                %Maximum allowed Hertzian pressure
-SF = 1.2;                             %Safety factor
-delta_hmax = delta_hlim/SF;         %Maximum allowed flank pressure [Pa]
-lb = 0.5;                           %Form factor, lower limit [~]
-ub = 5;                             %Form factor, upper limit [~]
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Static optimization for motor                                          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-k = 4e10*Cgr^2*Tpeak/(delta_hmax^2);%Introduced variable for gearbox [...]
-k_inv = 1/k;                        %Inverse of k [k^-1]
 
 cvx_begin gp
     variables l r n 
@@ -26,14 +13,5 @@ cvx_begin gp
         9 <= n <= 100;
         0.03 <= r <= 0.07;
         lb <= l/r <= ub;
-        
-        %Planetary gear
-%         n1 * power(rg1,-2) * power(bg1,-1) + (power(n1,-1)+ 2*power(n1,-2)+4*power(n1,-3)+8*power(n1,-4)+16*power(n1,-5)+32*power(n1,-6)+64*power(n1,-7))*power(rg1,-2)*power(bg1,-1) <= k_inv;
-%         n2 * power(rg2,-2) * power(bg2,-1)*power(n1,-1) + (power(n2,-1)+ 2*power(n2,-2)+4*power(n2,-3)+8*power(n2,-4)+16*power(n2,-5)+32*power(n2,-6)+64*power(n2,-7))*power(rg2,-2)*power(bg2,-1)*power(n1,-1) <= k_inv;
-%         rg1 == rg2;
-%         1/20 <= bg1/rg1 <= 4;
-%         1/20 <= bg2/rg2 <= 4;
-%         3 <= n1;
-%         3 <= n2;
-%         n1*n2*power(n,-1) ==1;
+
 cvx_end
